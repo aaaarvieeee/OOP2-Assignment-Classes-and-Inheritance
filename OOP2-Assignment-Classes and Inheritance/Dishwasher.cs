f6816641f0355abfe0c6a41573b745216ba932a5
aaaarvieeee <aaaarvieeee@users.noreply.github.com>
2023-02-09 10:14:52 -0700

    added the required classes and inheritances

---
+++ OOP2-Assignment-Classes and Inheritance/Dishwasher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2_Assignment_Classes_and_Inheritance
+{
+    internal class Dishwasher : Appliance
+    {
+    }
+}
